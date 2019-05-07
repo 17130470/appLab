9e339918e2a7fa7d705ecabd1a7a0ce7a0d0701f
17130470 <45854892+17130470@users.noreply.github.com>
2019-05-07 12:43:43 +0500

    Update test2.java

--- src/test/java/com/calculator/test2.java
+++ src/test/java/com/calculator/test2.java
@@ -1,30 +1,29 @@
 package com.calculator;
-/*Подключение библиотеки. JUnit - это библиотека, позволяющая
- * проводить модульное тестирование Java приложений. Класс Assert 
- * содержит набор утверждений, которые облегачют процесс тестирования. */
-import static org.junit.Assert.*;
-/*Аннтоация @Test указывает JUnit, что метод является тестовым методом*/
+
+/* Подключение библиотеки JUnit, позволяющая создавать тесты для программ */
+import org.junit.Assert;
 import org.junit.Test;
-/*Импортирует из пакета NalogC класс tax*/
-import com.calculator.Calculator;
-/*Объявляем новый класс test1*/
+
+/* Создание класса, содержащего код теста */
 public class test2 {
-	/*Обозначает новую переменную из класса tax*/
-	Calculator calc = new Calculator();
-	/*InterruptedException это проверяемое исключение, генерируемый многими методами стандартной библиотеки, которые блокируют поток исполнения*/
-	@Test
-	public void testCalc() throws InterruptedException
-	 {
-		/*Подставляет значения в метод calcTax1,
-		 * где высчитывают процент налога с дохода*/
-         calc.fields[0].setText("Hello, World!");
-         /* Запуск программы */
-         calc.calculate();
-         String r = calc.fields[1].getText();
-	 double res= Double.valueOf(r);
-		/*Заданное полученное значение*/
-	 double result=13.0;
-		/*Сравнивают высчитанное методом testCalc и заранее заданное значение */
-	 assertEquals(Double.toString(res),Double.toString(result));
-	 }
-}
+    
+	/* Аннотация @Test указывает JUnit, что в методе находится код теста */
+    @Test
+    public void testCalculator(){
+		/* Обработка исключения на случай возникновения ошибок в процессе выполнения теста */
+        try{
+		/* Создание объекта класса, содержащего программу */
+            Calculator lc = new Calculator();
+			/* Заполнение поля ввода программы определенной последовательностью символов */
+            lc.fields[0].setText("Hello, World!");
+			/* Запуск программы */
+            lc.calculate();
+			/* Сравнение результата, выдаваемого программой, с ожидаемым значением */
+            Assert.assertTrue("13".equals(lc.fields[1].getText()));
+        }
+		/* Код, выполняемый при возникновении ошибок в процессе выполнения теста */
+        catch(Exception e){
+			/* Неудачное завершение выполнения теста */
+            Assert.fail("Проверьте правильность входных значений");
+        }
+    }

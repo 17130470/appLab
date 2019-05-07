42e5f730c337a4d4d930021087ecfa75f917ded1
17130470 <45854892+17130470@users.noreply.github.com>
2019-05-07 11:35:22 +0500

    Update test2.java

--- src/test/java/com/calculator/test2.java
+++ src/test/java/com/calculator/test2.java
@@ -1,4 +1,4 @@
-package nalog.nak;
+package com.calculator;
 /*Подключение библиотеки. JUnit - это библиотека, позволяющая
  * проводить модульное тестирование Java приложений. Класс Assert 
  * содержит набор утверждений, которые облегачют процесс тестирования. */
@@ -6,21 +6,24 @@ import static org.junit.Assert.*;
 /*Аннтоация @Test указывает JUnit, что метод является тестовым методом*/
 import org.junit.Test;
 /*Импортирует из пакета NalogC класс tax*/
-import nalog.nak.tax;
+import com.calculator.Calculator;
 /*Объявляем новый класс test1*/
-public class test2 {
+public class test1 {
 	/*Обозначает новую переменную из класса tax*/
-	tax tax = new tax();
+	Calculator calc = new Calculator();
 	/*InterruptedException это проверяемое исключение, генерируемый многими методами стандартной библиотеки, которые блокируют поток исполнения*/
 	@Test
-	public void testTax() throws InterruptedException
+	public void testCalc() throws InterruptedException
 	 {
 		/*Подставляет значения в метод calcTax1,
 		 * где высчитывают процент налога с дохода*/
-	 double res= tax.calcTax(100, 13);
+         calc.fields[0].setText("Hello, World!");
+         /* Запуск программы */
+         String r = calc.fields[1].getText();
+	 double res= Double.valueOf(r);
 		/*Заданное полученное значение*/
-	 double toch=13.0;
-		/*Сравнивают высчитанное методом calcTax и заранее заданное значение */
-	 assertEquals(Double.toString(res),Double.toString(toch));
+	 double result=13.0;
+		/*Сравнивают высчитанное методом testCalc и заранее заданное значение */
+	 assertEquals(Double.toString(res),Double.toString(result));
 	 }
 }
